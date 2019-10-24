6409b3ab3bd7e462e94d3d4ac990b5a539afc359
Erick Augusto <erick.dev.it@gmail.com>
2019-10-24 01:12:50 -0300

    Implementando SellerService com metodo find all sincrono e iniciando a view index de vendedores

--- SalesWebMVC/Startup.cs
+++ SalesWebMVC/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebMVC.Data;
 using SalesWebMVC.Models;
+using SalesWebMVC.Services;
 
 namespace SalesWebMVC
 {
@@ -36,7 +37,9 @@ namespace SalesWebMVC
                     options.UseMySql(Configuration.GetConnectionString("SalesWebMVCContext"), builder =>
                     builder.MigrationsAssembly("SalesWebMVC")));
 
+            //Adicionando injecao de dependencia 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
